8b2926a894833edffd6b5b9a2ca1599a20f1af32
martinmt-mx <martin_4-3@hotmail.com>
2024-02-21 19:41:08 -0600

    Now the explore screen can display the list of all the available categories.

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,31 +1,70 @@
-import { View, TouchableOpacity, Text, ScrollView, StyleSheet, FlatList } from 'react-native';
+import { View, TouchableOpacity, Text, ScrollView, StyleSheet, Image } from 'react-native';
 import React, {useState} from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
+import Carousel from 'react-native-snap-carousel';
 
 const Explore = () => {
   const goToSelectCity = () => {
     console.log('go to select city');
   };
 
-  const goToCategory = (category) => {
+  const goToCategory = (category: string) => {
     console.log('go to category: ', category);
     setSelectedCategory(category);
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.slide}>
+      <Image style={styles.image} source={{ uri: item }} />
+    </View>
+  );
+
   const categories = [
-    { name: 'Departaments', icon: 'home' },
-    { name: 'Houses', icon: 'building' },
-    { name: 'Pool', icon: 'swimming-pool' },
-    { name: 'Outside', icon: 'tree' },
-    { name: 'Beach', icon: 'umbrella-beach' },
-    { name: 'Mountain', icon: 'mountain' },
+    { name: 'Departaments', icon: 'home', properties: [{ id: 1, name: 'Departamento 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },
+      { id: 2, name: 'Departamento 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
+    { name: 'Houses', icon: 'building', properties: [{ id: 2, name: 'Casa 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
+    { name: 'Pool', icon: 'building', properties: [{ id: 3, name: 'Alberca 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
+    { name: 'Outside', icon: 'building', properties: [{ id: 4, name: 'Outside 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
+    { name: 'Beach', icon: 'building', properties: [{ id: 5, name: 'Beach 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
+    { name: 'Mountain', icon: 'building', properties: [{ id: 6, name: 'Mountain 1', images: ['image1.jpg', 'image2.jpg', 'image3.jpg'], location: 'Ciudad de México', description: 'Acogedor departamento cerca del centro.', dateAvailable: 'Disponible ahora', price: '$100 por noche', rating: 4.5 },] },
   ];
 
 
   const [selectedCategory, setSelectedCategory] = useState(null);
 
+  const renderProperties = () => {
+    if (!selectedCategory) return null;
+    
+    const properties = categories.find(cat => cat.name === selectedCategory)?.properties || [];
+
+    return properties.map(property => (
+      <View key={property.id} style={styles.propertyContainer}>
+        <Carousel
+          data={property.images}
+          renderItem={renderItem}
+          sliderWidth={310}
+          itemWidth={300}
+        />
+        <View style={styles.favoriteButtonContainer}>
+          <TouchableOpacity style={styles.favoriteButton}>
+            <Icon name="heart" size={24} color="red" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.propertyInfo}>
+          <View style={styles.propertyHeader}>
+            <Text style={styles.propertyLocation}>{property.location}</Text>
+            <Text style={styles.propertyRating}>Rating: {property.rating}</Text>
+          </View>
+          <Text style={styles.propertyDescription}>{property.description}</Text>
+          <Text style={styles.propertyDateAvailable}>{property.dateAvailable}</Text>
+          <Text style={styles.propertyPrice}>{property.price}</Text>
+        </View>
+      </View>
+    ));
+  };
 
   return (
+    <ScrollView >
     <View style={styles.container}>
       <View style={styles.searchContainer}>
         <TouchableOpacity onPress={goToSelectCity} style={styles.searchButton}>
@@ -52,10 +91,18 @@ const Explore = () => {
               </TouchableOpacity>
             ))}
           </ScrollView>
+          <View >
+            <View style={styles.propertiesContainer}>
+              {renderProperties()}
+            </View>
+          </View>
+          
         </View>
       </View>
 
     </View>
+    </ScrollView>
+
   );
 }
 
@@ -100,8 +147,8 @@ const styles = StyleSheet.create({
   categoryButton: {
     backgroundColor: 'transparent',
     paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginRight: 10,
+    paddingHorizontal: 10,
+    marginRight: 15,
   },
   categoryText: {
     fontSize: 16,
@@ -113,6 +160,66 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: 'black',
     
+  },
+  propertiesContainer: {
+    alignContent: 'center',
+    marginTop: 20,
+    marginHorizontal: 35,
+  },
+  propertyContainer: {
+    marginBottom: 20,
+  },
+  propertyInfo: {
+    marginTop: 10,
+  },
+  propertyLocation: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  propertyDescription: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+  propertyDateAvailable: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+  propertyPrice: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+  propertyRating: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  slide: {
+    width: 305,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  favoriteButtonContainer: {
+    position: 'absolute',
+    top: 10, 
+    right: 30, 
+  },
+  favoriteButton: {
+    padding: 5,
+  },
+  propertyHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
 
