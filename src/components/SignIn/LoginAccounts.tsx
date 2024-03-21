eaf333a9bd0e1b26198c8fe82301b94e4b094a25
Diana Campos <dcampos0495@gmail.com>
2024-03-20 19:44:22 -0600

    Add SignIn and SignOut with Google

--- src/components/SignIn/LoginAccounts.tsx
+++ src/components/SignIn/LoginAccounts.tsx
@@ -1,20 +1,45 @@
 /* eslint-disable */
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth'
+import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import LineDesign from './LineDesign';
 
+const signinWithGoogle = async () => {
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    const { idToken } = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    const user = await auth().signInWithCredential(googleCredential);
+    console.log(user)
+} 
+GoogleSignin.configure({
+    webClientId: '950063643166-imhvslitr5ti6q900i4sqqmgg8oe4gat.apps.googleusercontent.com',
+  });
+
 const LoginAccounts = () => {
+    const navigation = useNavigation();
+    const [user, setUser] = useState<FirebaseAuthTypes.User|null >(null);
+    const handleGoogleSignIn = async () => {
+        try {
+            await signinWithGoogle();
+            setUser(auth().currentUser);
+            navigation.navigate('Main')
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <View style={styles.container}>
         <LineDesign/>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => handleGoogleSignIn()}>
             <View style={styles.login_btn}>
                 <Icon name="logo-google" size={27} color="#A663CC" style={styles.icon}/>
                 <Text style={styles.text}>Continue with Google</Text>
             </View>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
             <View style={styles.login_btn}>
                 <Icon name="logo-facebook" size={27} color="#A663CC" style={styles.icon}/>
                 <Text style={styles.text}>Continue with Facebook</Text>
