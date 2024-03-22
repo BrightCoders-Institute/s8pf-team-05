e1b5b831bb3692818c14aceb437944e894350892
AxeLoza21 <osc.axeloza@gmail.com>
2024-03-21 21:51:32 -0600

    Modification of components
    The profile picture component has been modified so that it can be reused in the form to become a host.
    The Personal Information screen was readapted to adapt it to the component modification.

--- src/screens/PersonalInformation.tsx
+++ src/screens/PersonalInformation.tsx
@@ -1,47 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Profilepicture from '../components/personalInfo/Profilepicture'
-import InfoPersonal from '../components/personalInfo/InfoPersonal'
-import  Icon  from 'react-native-vector-icons/AntDesign'
-import HeaderNavigation from '../navigation/HeaderNavigation'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Profilepicture from '../components/personalInfo/Profilepicture';
+import InfoPersonal from '../components/personalInfo/InfoPersonal';
+import Icon from 'react-native-vector-icons/AntDesign';
+import HeaderNavigation from '../navigation/HeaderNavigation';
 
 const PersonalInformation = () => {
   return (
     <View style={styles.container}>
-      <HeaderNavigation whereNav='Profile'/>
+      <HeaderNavigation whereNav="Profile" />
       <Text style={styles.text}>Personal information</Text>
       <View style={styles.profile}>
-        <Profilepicture/>
+        <Profilepicture />
       </View>
-      <InfoPersonal 
-        name='Nombre legal' 
-        value='Juancho Perez Debian Soto'
-        action='Editar'/>
-      <InfoPersonal 
-        name='Numero telefonico' 
-        value='+52 *** *** 1290'
-        action='Editar'/>
-      <InfoPersonal 
-        name='Direeccion' 
-        value='No proporcionada'
-        action='Agregar'/>
+      <InfoPersonal
+        name="Nombre legal"
+        value="Juancho Perez Debian Soto"
+        action="Editar"
+      />
+      <InfoPersonal
+        name="Numero telefonico"
+        value="+52 *** *** 1290"
+        action="Editar"
+      />
+      <InfoPersonal
+        name="Direeccion"
+        value="No proporcionada"
+        action="Agregar"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default PersonalInformation
+export default PersonalInformation;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
+  container: {
+    flex: 1,
+    padding: 20,
   },
-  text:{
+  text: {
     fontSize: 30,
-    marginBottom: 20
+    marginBottom: 20,
   },
-  profile:{
+  profile: {
     justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    alignItems: 'center',
+  },
+});
