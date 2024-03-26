aa75745c2a421bf4ecc697767e60d1948efe0950
martinmt-mx <martin_4-3@hotmail.com>
2024-03-26 13:27:00 -0600

    The Detail Screen now display the host information. The host information is displayed in a card with the host's name, profile picture, and a brief description of the host.

--- src/screens/PropertyDetails.tsx
+++ src/screens/PropertyDetails.tsx
@@ -1,62 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import CarouselComponent from '../components/PropertyDetails/Carousel';
 import RatingBox from '../components/PropertyDetails/RatingBox';
 import HostInfo from '../components/PropertyDetails/HostInfo';
 import PropertyBottomTab from '../components/PropertyDetails/PropertyBottomTab';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 const PropertyDetailsScreen: React.FC = ({ route }: any) => {
-
   const navigation = useNavigation();
-
   const { property } = route.params;
 
-  const {
-    images,
-    propertyName,
-    location,
-    guests,
-    bedrooms,
-    beds,
-    bathrooms,
-    averageRating,
-    totalReviews,
-    hostName,
-    hostImage,
-    description,
-    price,
-  } = property;
+  const [hostInfo, setHostInfo] = useState<{ name: string, lastname: string, profileImage: string | null }>({
+    name: '',
+    lastname: '',
+    profileImage: null,
+  });
+
+  useEffect(() => {
+    const fetchHostInfo = async () => {
+      try {
+        const hostId = property.hostId;
+
+        if (hostId) {
+          const userDoc = await firestore().collection('users').doc(hostId).get();
+          
+          const userData = userDoc.data();
+
+          if (userData) {
+            const { name, lastname, profileImage } = userData;
+            setHostInfo({ name, lastname, profileImage });
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching host info: ', error);
+      }
+    };
+
+    fetchHostInfo();
+  }, [property.hostId]);
 
   const handleReservePress = () => {
     navigation.navigate('ConfirmReservation');
   };
 
-return (
+  return (
     <View style={styles.container}>
-        <ScrollView>
-            <View>
-                <CarouselComponent images={images} />
-            </View>
-            <View style={styles.propertyInfo}>
-                <Text style={styles.propertyName}>{propertyName}</Text>
-                <Text style={styles.propertyLocation}>{location}</Text>
-                <Text style={styles.propertyDetails}>
-                    Guests: {guests} | Bedrooms: {bedrooms} | Beds: {beds} | Bathrooms: {bathrooms}
-                </Text>
-                {/* <RatingBox averageRating={averageRating} totalReviews={totalReviews} /> */}
-                {/* <HostInfo hostName={hostName} hostImage={hostImage} /> */}
-            </View>
-            <View style={styles.propertyDescription}>
-                <Text style={styles.descriptionTitle}>Description:</Text>
-                <Text style={styles.descriptionText}>{description}</Text>
-            </View>
-        </ScrollView>
-        <View style={styles.bottomTab}>
-            <PropertyBottomTab pricePerNight={price} onReservePress={handleReservePress} />
+      <ScrollView>
+        <View>
+          <CarouselComponent images={property.images} />
         </View>
+        <View style={styles.propertyInfo}>
+          <Text style={styles.propertyName}>{property.propertyName}</Text>
+          <Text style={styles.propertyLocation}>{property.location}</Text>
+          <Text style={styles.propertyDetails}>
+            Guests: {property.guests} | Bedrooms: {property.bedrooms} | Beds: {property.beds} | Bathrooms: {property.bathrooms}
+          </Text>
+          {/*<RatingBox averageRating={property.averageRating} totalReviews={property.totalReviews} />*/}
+          <HostInfo hostName={`${hostInfo.name} ${hostInfo.lastname}`} hostImage={hostInfo.profileImage} />
+        </View>
+        <View style={styles.propertyDescription}>
+          <Text style={styles.descriptionTitle}>Description:</Text>
+          <Text style={styles.descriptionText}>{property.description}</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.bottomTab}>
+        <PropertyBottomTab pricePerNight={property.price} onReservePress={handleReservePress} />
+      </View>
     </View>
-);
+  );
 };
 
 const styles = StyleSheet.create({
