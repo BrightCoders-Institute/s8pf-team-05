dcc39b8bfa2d5aab9c53f4eb8018f38c0e7ecbaf
martinmt-mx <martin_4-3@hotmail.com>
2024-04-26 02:27:53 -0600

    The ReviewScreen now displays the reviews for a reservation

--- src/screens/ReviewScreen.tsx
+++ src/screens/ReviewScreen.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
-import ReviewForm from '../components/Review/ReviewForm'; 
-import ReviewItem from '../components/Review/ReviewItem'; 
-import { useNavigation, useRoute } from '@react-navigation/native';
+import ReviewForm from '../components/Review/ReviewForm';
+import ReviewItem from '../components/Review/ReviewItem';
+import { useNavigation } from '@react-navigation/native';
 import firestore from '@react-native-firebase/firestore';
-import  Icon  from 'react-native-vector-icons/AntDesign'
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const ReviewScreen: React.FC = ({ route }: any) => {
   const navigation = useNavigation();
   const { property } = route.params;
 
-  const [reviews, setReviews] = React.useState<any[]>([]);
-  const [loading, setLoading] = React.useState(true);
+  const [reviews, setReviews] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchReviews = async () => {
       try {
         const reviewsSnapshot = await firestore()
+          .collection('properties')
+          .doc(property.id)
           .collection('reviews')
-          .where('propertyId', '==', property.id)
           .get();
-        
+
         const fetchedReviews = reviewsSnapshot.docs.map(doc => doc.data());
         setReviews(fetchedReviews);
         setLoading(false); // Mark loading as false once reviews are fetched
@@ -33,6 +34,9 @@ const ReviewScreen: React.FC = ({ route }: any) => {
     fetchReviews();
   }, [property.id]);
 
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
 
   if (loading) {
     return (
@@ -42,15 +46,11 @@ const ReviewScreen: React.FC = ({ route }: any) => {
     );
   }
 
-  const handleGoBack = () => {
-    navigation.goBack();
-  };
-
   return (
     <>
       <View style={styles.button_container}>
         <TouchableOpacity onPress={handleGoBack}>
-          <Icon name ='arrowleft' size={27} color={'black'}/>
+          <Icon name="arrowleft" size={27} color={'black'} />
         </TouchableOpacity>
       </View>
       <View style={styles.container}>
@@ -59,18 +59,17 @@ const ReviewScreen: React.FC = ({ route }: any) => {
           <View style={styles.formContainer}>
             <ReviewForm propertyId={property.id} />
           </View>
-          {reviews.length === 0 ? ( // Verificar si no hay reviews
+          {reviews.length === 0 ? (
             <View style={styles.emptyContainer}>
-              
               <Image source={require('../images/empty-state-reviews.png')} style={styles.emptyImage} />
-              <Text style={styles.emptyText}>There is no reviews yet</Text>
+              <Text style={styles.emptyText}>There are no reviews yet</Text>
             </View>
           ) : (
             <>
               <Text style={styles.reviewsTitle}>Users Reviews</Text>
               <View style={styles.reviewsContainer}>
-                {reviews.map(review => (
-                  <ReviewItem key={review.id} review={review} />
+                {reviews.map((review, index) => (
+                  <ReviewItem key={index} review={review} />
                 ))}
               </View>
             </>
@@ -107,7 +106,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   formContainer: {
-    alignItems: 'center', 
+    alignItems: 'center',
     marginBottom: 20,
   },
   reviewsContainer: {
