50f34a48c0373bb5abc5dbd00f2c9f5e70787274
martinmt-mx <martin_4-3@hotmail.com>
2024-05-12 15:05:40 -0600

    I added a loading screen to the PropertyReservations, Trips, and Wishlist screens.

--- src/screens/Trips.tsx
+++ src/screens/Trips.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import {StyleSheet, Text, View, ScrollView, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import CardTrip from '../components/TripsScreenComponents/CardTrip';
 import EmptyState from '../components/EmptyState';
@@ -66,9 +66,7 @@ const Trips = () => {
   return (
     <View style={styles.container}>
       {loading ? (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
+        <Text style={styles.title}>Loading...</Text>
       ) : (
         <>
           <Text style={styles.title}>Next trips</Text>
@@ -153,11 +151,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 20,
     marginBottom: 15,
-  },
-  loadingContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
--- src/screens/Wishlist.tsx
+++ src/screens/Wishlist.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable */
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import EmptyState from '../components/EmptyState';
 
 const CardFavorites = () => {
   const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const fetchFavorites = async () => {
@@ -18,7 +18,7 @@ const CardFavorites = () => {
         const likesSnapshot = await userRef.collection('likes').get();
         const likedPropertyIds = likesSnapshot.docs.map(doc => doc.id);
         const favoritesData = [];
-        
+
         for (const propertyId of likedPropertyIds) {
           const propertyRef = firestore().collection('properties').doc(propertyId);
           const propertySnapshot = await propertyRef.get();
@@ -31,104 +31,113 @@ const CardFavorites = () => {
         }
 
         setFavorites(favoritesData);
+        setLoading(false); // Step 4: Update loading state when data fetching is complete
       } catch (error) {
         console.error('Error fetching favorites: ', error);
       }
     };
 
     fetchFavorites();
-  }, [favorites]);
+  }, []); 
 
   return (
     <ScrollView>
-      <View style={styles.containerPrincipal}>
-        <Text style={styles.header}>Wishlist</Text>
-        {favorites.length === 0 ? (
-          <EmptyState
-            imageSource={require('../images/empty-state-properties-list.png')}
-            message="You haven't liked any property yet."
-          />
-        ) : (
-          favorites.map((favorite, index) => (
-          <View key={index} style={styles.container}>
-            <Image style={styles.img} source={{ uri: favorite.images[0] }} />
-            <View style={styles.container_description}>
-              <Text style={styles.textName} numberOfLines={1}>{favorite.propertyName}</Text>
-              <Text style={styles.textLocation} numberOfLines={1}>{ favorite.propertyAdress || favorite.location}</Text>
-              <Text style={styles.textPrice} numberOfLines={1}>$ {favorite.price}.00 night</Text>
-              <Text style={styles.textDescription} numberOfLines={1}> {favorite.description}</Text>
-            </View>
-          </View>
-        ))
-      )
-    }
-      </View>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+        <View style={styles.containerPrincipal}>
+          <Text style={styles.header}>Wishlist</Text>
+          {favorites.length === 0 ? (
+            <EmptyState
+              imageSource={require('../images/empty-state-properties-list.png')}
+              message="You haven't liked any property yet."
+            />
+          ) : (
+            favorites.map((favorite, index) => (
+              <View key={index} style={styles.container}>
+                <Image style={styles.img} source={{ uri: favorite.images[0] }} />
+                <View style={styles.container_description}>
+                  <Text style={styles.textName} numberOfLines={1}>{favorite.propertyName}</Text>
+                  <Text style={styles.textLocation} numberOfLines={1}>{ favorite.propertyAdress || favorite.location}</Text>
+                  <Text style={styles.textPrice} numberOfLines={1}>$ {favorite.price}.00 night</Text>
+                  <Text style={styles.textDescription} numberOfLines={1}> {favorite.description}</Text>
+                </View>
+              </View>
+            ))
+          )}
+        </View>
+      )}
     </ScrollView>
-  )
+  );
 }
 
-export default CardFavorites
+export default CardFavorites;
 
 const styles = StyleSheet.create({
-    header:{
-      color: '#444444',
-      fontSize: 30,
-      fontWeight: 'bold',
-      marginBottom: 20,
-    },
-    containerPrincipal:{
-      paddingHorizontal: 30,
-      paddingVertical: 20,
-    },
-    container:{
-      flexDirection:'row',
-      marginBottom:15,
-      
-    },
-    img:{
-      width: 100,
-      height:100,
-      borderBottomLeftRadius: 10,
-      borderTopLeftRadius: 10,
-    },
-    container_description:{
-      flex:1,
-      borderBottomRightRadius: 10,
-      borderTopRightRadius: 10,
-      alignItems: 'flex-start',
-      elevation: 10,
-      backgroundColor: 'white'
-    },
-    description_text:{
-      fontSize:12,
-    },
-    textName:{
-      color:'#444444',
-      fontWeight: '500',
-      fontSize: 16,
-      paddingLeft: 10,
-      marginTop: 10,
-    },
-    textLocation:{
-      fontSize: 12,
-      color: '#7C7C7C',
-      textAlign:'justify',
-      paddingLeft:10,
-    },
-    textPrice: {
-      color: '#575757',
-      fontWeight: '500',
-      textAlign:'justify',
-      paddingLeft:10,
-    },
-    textDescription:{
-      fontSize: 11,
-      color: '#7C7C7C',
-      textAlign:'justify',
-      paddingLeft:10,
-    },
-    container_btn:{
-      position:'absolute',
-    }
-  })
-
+  header: {
+    color: '#444444',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  containerPrincipal: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+  container: {
+    flexDirection: 'row',
+    marginBottom: 15,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderBottomLeftRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  container_description: {
+    flex: 1,
+    borderBottomRightRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: 'flex-start',
+    elevation: 10,
+    backgroundColor: 'white',
+  },
+  description_text: {
+    fontSize: 12,
+  },
+  textName: {
+    color: '#444444',
+    fontWeight: '500',
+    fontSize: 16,
+    paddingLeft: 10,
+    marginTop: 10,
+  },
+  textLocation: {
+    fontSize: 12,
+    color: '#7C7C7C',
+    textAlign: 'justify',
+    paddingLeft: 10,
+  },
+  textPrice: {
+    color: '#575757',
+    fontWeight: '500',
+    textAlign: 'justify',
+    paddingLeft: 10,
+  },
+  textDescription: {
+    fontSize: 11,
+    color: '#7C7C7C',
+    textAlign: 'justify',
+    paddingLeft: 10,
+  },
+  container_btn: {
+    position: 'absolute',
+  },
+  loadingContainer: { 
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
